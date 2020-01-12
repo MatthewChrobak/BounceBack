f49d71cb5086869c92446a8b29b0142dd02c4863
Ouillyang <william.huynh22@gmail.com>
2020-01-11 19:03:04 -0500

    Timer bar and text

--- BounceBack/BounceBack/Scenes/FirstScene.cs
+++ BounceBack/BounceBack/Scenes/FirstScene.cs
@@ -10,6 +10,7 @@ using BounceBack.Scenes.Elements;
 using BounceBack.Containers;
 using Annex.Graphics.Contexts;
 using Annex.Data.Shared;
+using Annex.Data;
 
 namespace BounceBack.Scenes
 {
@@ -25,6 +26,8 @@ namespace BounceBack.Scenes
 
         private TextureContext _bouncer;
         private TextureContext _timerBar;
+        private TextureContext _timerBarBackground;
+        private TextContext _timerText;
 
 
         private readonly ActionButtonContainer _actionButtonContainer;
@@ -62,8 +65,21 @@ namespace BounceBack.Scenes
             this.Events.AddEvent("timer", PriorityType.ANIMATION, () =>
             {
                 if(timeLimitCount >= 1)
-                {                    
-                    _timerBar.RenderSize = new ScalingVector(Vector.Create(150, 50), Vector.Create(GetTimeRatio(), 1));
+                {
+                    if (GetTimeRatio() <= 0.2f)
+                    {
+                        _timerBar.RenderColor = RGBA.Red;
+                    }
+                    else if (GetTimeRatio() <= 0.5f)
+                    {
+                        _timerBar.RenderColor = RGBA.Yellow;
+                    }
+                    else
+                    {
+                        _timerBar.RenderColor = RGBA.Green;
+                    }
+                    _timerBar.RenderSize = new ScalingVector(Vector.Create(150, 25), Vector.Create(GetTimeRatio(), 1));
+                    _timerText.RenderText = System.String.Format("{0:0}", System.Math.Ceiling(GetTimeRatio() * 5));
                 }
                 return ControlEvent.NONE;
             }, 100);
@@ -95,7 +111,9 @@ namespace BounceBack.Scenes
         public override void Draw(ICanvas canvas)
         {
             this._casinoQueue.Draw(canvas);
+            canvas.Draw(this._timerBarBackground);
             canvas.Draw(this._timerBar);
+            canvas.Draw(this._timerText);
             canvas.Draw(this._bouncer);
             base.Draw(canvas);
         }
@@ -117,10 +135,29 @@ namespace BounceBack.Scenes
             this.Events.AddEvent(_timelimitEvent, PriorityType.LOGIC, TimeLimit, interval_ms: _timeLimit);
             _timerBar = new TextureContext("background.png")
             {
-                RenderPosition = Vector.Create(ServiceProvider.Canvas.GetResolution().X - 250, 100),
-                RenderSize = new ScalingVector(Vector.Create(150, 50), Vector.Create(GetTimeRatio(), 1)),
+                RenderPosition = Vector.Create(ServiceProvider.Canvas.GetResolution().X - 250, 150),
+                RenderSize = new ScalingVector(Vector.Create(150, 25), Vector.Create(GetTimeRatio(), 1)),
                 UseUIView = true
-                //SourceTextureRect = new IntRect(0, 0, new ScalingInt(150, (int)(1 - GetTimeRatio())), 100)                
+            };
+            _timerBarBackground = new TextureContext("background.png")
+            {
+                RenderPosition = Vector.Create(ServiceProvider.Canvas.GetResolution().X - 250, 150),
+                RenderSize = Vector.Create(150, 25),
+                RenderColor = RGBA.Black,
+                UseUIView = true
+            };
+            _timerText = new TextContext("5.0", "default.ttf")
+            {
+                RenderPosition = Vector.Create(ServiceProvider.Canvas.GetResolution().X - 250, 100),
+                FontSize = 30,
+                FontColor = RGBA.White,
+                UseUIView = true,
+                Alignment = new TextAlignment()
+                {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Size = Vector.Create(150,100)
+                }
             };
         }
 
